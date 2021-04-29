0513a16be61b7be687af2740f800d247c1870847
double-six <1922685869@qq.com>
2021-04-29 15:52:41 +0800

    1.21兼容链式运动

--- move.js
+++ move.js
@@ -1,16 +1,21 @@
 
-			function move(obj,name,target) {
+			function move(obj,name,target,fn) {
 				clearInterval(obj.timerA)
 				obj.timerA = setInterval(function() {
 					let nowSet=parseInt(getComputedStyle(obj)[name])
 					let speed=(target-nowSet)/10
 					if(speed>0){
+						//向上取整(整数位+1，省去小数部分)
 						speed=Math.ceil(speed)
 					}else{
+						//向下取整(省去小数部分)
 						speed=Math.floor(speed)
 					}
 					if (nowSet == target) {
 						clearInterval(obj.timerA)
+						if(fn){
+							fn()
+						}
 					} else {
 						// var speed=(target-nowSet)/10
 						obj.style[name] = nowSet + speed+ 'px'
