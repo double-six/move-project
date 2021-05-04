e6cdcc5987e5f835c5e3a8d489717c3478c2c813
double-six <1922685869@qq.com>
2021-05-04 16:28:17 +0800

    move链式运动1.22

--- move.js
+++ move.js
@@ -1,24 +1,27 @@
+//move链式运动1.3
+function move(obj, name, target, fn) {
+	clearInterval(obj.timerA)
+	obj.timerA = setInterval(function() {
+		if (name == "opacity") {
+			nowSet = getComputedStyle(obj)[name] * 100
+		} else {
+			nowSet = parseInt(getComputedStyle(obj)[name])
+		}
+		let speed = (target - nowSet) / 10
 
-			function move(obj,name,target,fn) {
-				clearInterval(obj.timerA)
-				obj.timerA = setInterval(function() {
-					let nowSet=parseInt(getComputedStyle(obj)[name])
-					let speed=(target-nowSet)/10
-					if(speed>0){
-						//向上取整(整数位+1，省去小数部分)
-						speed=Math.ceil(speed)
-					}else{
-						//向下取整(省去小数部分)
-						speed=Math.floor(speed)
-					}
-					if (nowSet == target) {
-						clearInterval(obj.timerA)
-						if(fn){
-							fn()
-						}
-					} else {
-						// var speed=(target-nowSet)/10
-						obj.style[name] = nowSet + speed+ 'px'
-					}
-				}, 30)
+		//三元运算符
+		speed = (speed > 0) ? Math.ceil(speed) : Math.floor(speed)
+		if (nowSet == target) {
+			clearInterval(obj.timerA)
+			if (fn) {
+				fn()
 			}
+		} else {
+			if (name == "opacity") {
+				obj.style[name] = (nowSet + speed) / 100
+			} else {
+				obj.style[name] = nowSet + speed + 'px'
+			}
+		}
+	}, 30)
+}
