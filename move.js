60cf8eeadfc3bf4c7744c14451204a27aa41ae28
double-six <1922685869@qq.com>
2021-05-12 15:24:34 +0800

    move.js1.3版链式运动兼容各种样式

--- move.js
+++ move.js
@@ -1,11 +1,13 @@
-//move链式运动1.22
+//move链式运动1.3
 function move(obj, name, target, fn) {
 	clearInterval(obj.timerA)
 	obj.timerA = setInterval(function() {
 		if (name == "opacity") {
 			nowSet = getComputedStyle(obj)[name] * 100
-		} else {
+		} else if (name == "opacity"||name == "font-size"||name == "width"||name == "height"||name == "border-width"||name == "border-radius") {
 			nowSet = parseInt(getComputedStyle(obj)[name])
+		}else{
+			nowSet = getComputedStyle(obj)[name]     //获取obj属性的属性值
 		}
 		let speed = (target - nowSet) / 10
 
@@ -19,8 +21,10 @@ function move(obj, name, target, fn) {
 		} else {
 			if (name == "opacity") {
 				obj.style[name] = (nowSet + speed) / 100
-			} else {
+			} else if (name == "opacity"||name == "font-size"||name == "width"||name == "height"||name == "border-width"||name == "border-radius"){
 				obj.style[name] = nowSet + speed + 'px'
+			}else{
+				obj.style[name] =target
 			}
 		}
 	}, 30)
